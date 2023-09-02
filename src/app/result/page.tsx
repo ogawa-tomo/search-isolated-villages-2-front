09dc1a553a33533ac78d04ac6d207a28d9754ee9
tomo <o_tomo_1990@yahoo.co.jp>
2023-09-02 22:06:09 +0900

    人口などのデータを取得

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -8,13 +8,16 @@ const Page = async ({ searchParams }) => {
       <h1>秘境集落探索ツール</h1>
       <h2>探索結果</h2>
       <div>{searchParams.region}</div>
-      {/* <div>{villages[0].name}</div> */}
-      <ul>
-        {villages.map((village, index) => (
-          <li key={index}>{index + 1}位: {village.pref} {village.city} {village.district}</li>
-          // <li key={index}>{typeof(village)} {village.pref} hoge</li>
-        ))}
-      </ul>
+      {villages.map((village, index) => (
+        <p key={index}>
+          {index + 1}位<br />
+          {village.pref} {village.city} {village.district}<br />
+          人口: {village.population}人<br />
+          都会度: {village.urban_point}<br />
+          <a href={village.google_map_url} target="_blank">Googleマップ</a><br />
+          <a href={`http://localhost:5000${village.mesh_map_path}`} target="_blank">人口分布図</a>
+        </p>
+      ))}
     </>
   )
 }
