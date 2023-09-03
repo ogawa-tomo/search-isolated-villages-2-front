1d554ffda4f188e76ad5734acbb0afc1907b1c2a
tomo <o_tomo_1990@yahoo.co.jp>
2023-09-03 22:37:15 +0900

    suspenseを追加

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,6 @@
-
-type villageSearchParams = {
-  region: string
-}
+import VillageList from "@/components/VillageList";
+import { Suspense } from "react";
+import { villageSearchParams } from "@/types/villageSearchParams";
 
 const fetchVillages = async (params: villageSearchParams) => {
   const query = new URLSearchParams(params)
@@ -15,22 +14,14 @@ const fetchVillages = async (params: villageSearchParams) => {
 }
 
 const Page = async ({ searchParams }) => {
-  const villages = await fetchVillages(searchParams);
   return (
     <>
       <h1>秘境集落探索ツール</h1>
       <h2>探索結果</h2>
       <div>{searchParams.region}</div>
-      {villages.map((village, index) => (
-        <p key={index}>
-          {index + 1}位<br />
-          {village.pref} {village.city} {village.district}<br />
-          人口: {village.population}人<br />
-          都会度: {village.urban_point}<br />
-          <a href={village.google_map_url} target="_blank">Googleマップ</a><br />
-          <a href={`http://localhost:5000${village.mesh_map_path}`} target="_blank">人口分布図</a>
-        </p>
-      ))}
+      <Suspense fallback='loading...'>
+        <VillageList region={searchParams.region} />
+      </Suspense>
     </>
   )
 }
---
+++ src/components/VillageList.tsx
@@ -0,0 +1,34 @@
+import { villageSearchParams } from "@/types/villageSearchParams"
+
+const fetchVillages = async (params: villageSearchParams) => {
+  const query = new URLSearchParams(params)
+  const response = await fetch(
+    `http://localhost:5000/api/result?${query}`,{
+      method: 'GET',
+      cache: 'no-store'
+    }
+  )
+  return response.json()
+}
+
+const VillageList = async (searchParams: villageSearchParams) => {
+  const villages = await fetchVillages(searchParams);
+
+  return (
+    <>
+      {villages.map((village, index) => (
+        <p key={index}>
+          {index + 1}位<br />
+          {village.pref} {village.city} {village.district}<br />
+          人口: {village.population}人<br />
+          都会度: {village.urban_point}<br />
+          <a href={village.google_map_url} target="_blank">Googleマップ</a><br />
+          <a href={`http://localhost:5000${village.mesh_map_path}`} target="_blank">人口分布図</a>
+        </p>
+      ))}
+    </>
+  )
+
+}
+
+export default VillageList
