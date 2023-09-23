56434095e11baa2870b403151c62d69659fc67c3
tomo <o_tomo_1990@yahoo.co.jp>
2023-09-24 08:54:37 +0900

    探索結果を整える

--- src/components/VillageList.tsx
+++ src/components/VillageList.tsx
@@ -16,16 +16,28 @@ const VillageList = async (searchParams: VillageSearchParams) => {
 
   return (
     <>
-      {villages.map((village, index) => (
-        <p key={index}>
-          {index + 1}位<br />
-          {village.pref} {village.city} {village.district}<br />
-          人口: {village.population}人<br />
-          都会度: {village.urban_point}<br />
-          <a href={village.google_map_url} target="_blank">Googleマップ</a><br />
-          <a href={`http://localhost:5000${village.mesh_map_path}`} target="_blank">人口分布図</a>
-        </p>
-      ))}
+      <div className="flex flex-col items-center">
+        <div className="w-96">
+          <table className="table border-collapse border border-slate-400">
+            <tbody>
+              {villages.map((village, index) => (
+                <tr>
+                  <td>
+                    {index + 1}位
+                  </td>
+                  <td className="columns-xs ">
+                    <span className="font-bold text-lg">{village.pref} {village.city} {village.district}</span><br />
+                    <span className="mr-1">人口: {village.population}人</span>
+                    <span>都会度: {village.urban_point}</span><br />
+                    <a className="mr-1" href={village.google_map_url} target="_blank">Googleマップ</a>
+                    <a href={`http://localhost:5000${village.mesh_map_path}`} target="_blank">人口分布図</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   )
 
