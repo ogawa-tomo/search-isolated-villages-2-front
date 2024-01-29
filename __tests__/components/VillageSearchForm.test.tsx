9e9c6f17af868aa6bcd807be28bb0320c43872b2
tomo <o_tomo_1990@yahoo.co.jp>
2024-01-29 21:17:12 +0900

    地域とオプションを選択して検索する

--- __tests__/components/VillageSearchForm.test.tsx
+++ __tests__/components/VillageSearchForm.test.tsx
@@ -1,6 +1,7 @@
 import { getByLabelText, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
+import Modal from 'react-modal';
 import VillageSearchForm from '@/components/VillageSearchForm';
 import selectEvent from 'react-select-event';
 
@@ -14,6 +15,8 @@ jest.mock('next/navigation', () => ({
     return { push: mockFn }
   }
 }));
+
+// Modal
 
 describe('VillageSearchForm', () => {
   beforeEach(() => {
@@ -44,38 +47,37 @@ describe('VillageSearchForm', () => {
 
   it('地域とオプションを選択して検索する', async () => {
     render(<VillageSearchForm />);
-    const button = screen.getByRole('button', { name: '探索' });
+
     const regionSelectBox = screen.getByRole('combobox');
     await selectEvent.select(regionSelectBox, '青森県');
 
-    const openOptionModalButton = screen.getByRole('button', { name: '詳細条件' });
-    await user.click(openOptionModalButton);
+    await user.click(screen.getByRole('button', { name: '詳細条件' }));
 
     expect(screen.getByRole('group', { name: '人口' })).toBeInTheDocument();
     const populationLowerLimitTextbox = screen.getByRole('spinbutton', { name: '最小：' });
-    await user.type(populationLowerLimitTextbox, '200')
+    await user.clear(populationLowerLimitTextbox);
+    await user.type(populationLowerLimitTextbox, '200');
     const populationUpperLimitTextbox = screen.getByRole('spinbutton', { name: '最大：' });
-    await user.type(populationUpperLimitTextbox, '500')
+    await user.clear(populationUpperLimitTextbox);
+    await user.type(populationUpperLimitTextbox, '500');
 
     expect(screen.getByRole('group', { name: '離島設定' })).toBeInTheDocument();
     await user.click(screen.getByLabelText('離島のみ'));
 
+    await user.type(screen.getByRole('textbox', { name: 'キーワード絞り込み' }), '佐井村');
 
-
-
-
-    expect(screen.getByRole('button', { name: 'デフォルト値に戻す' })).toBeInTheDocument();
+    await user.click(screen.getByRole('button', { name: '閉じる' }));
 
     const params = new URLSearchParams({
       region: '青森県',
-      populationLowerLimit: '1',
-      populationUpperLimit: '10000',
-      islandSetting: '離島を含まない',
-      keyWords: '',
+      populationLowerLimit: '200',
+      populationUpperLimit: '500',
+      islandSetting: '離島のみ',
+      keyWords: '佐井村',
       page: '1'
     });
 
-    // await user.click(button);
-    // expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
+    await user.click(screen.getByRole('button', { name: '探索' }));
+    expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
   })
 });
