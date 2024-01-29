ec0706be5daeaa7ff9428072e1a11f2100d963a7
tomo <o_tomo_1990@yahoo.co.jp>
2024-01-29 21:20:46 +0900

    デフォルト値に戻すテスト

--- __tests__/components/VillageSearchForm.test.tsx
+++ __tests__/components/VillageSearchForm.test.tsx
@@ -80,4 +80,41 @@ describe('VillageSearchForm', () => {
     await user.click(screen.getByRole('button', { name: '探索' }));
     expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
   })
+
+  it('地域とオプションを選択した後、デフォルト値に戻して検索する', async () => {
+    render(<VillageSearchForm />);
+
+    const regionSelectBox = screen.getByRole('combobox');
+    await selectEvent.select(regionSelectBox, '青森県');
+
+    await user.click(screen.getByRole('button', { name: '詳細条件' }));
+
+    expect(screen.getByRole('group', { name: '人口' })).toBeInTheDocument();
+    const populationLowerLimitTextbox = screen.getByRole('spinbutton', { name: '最小：' });
+    await user.clear(populationLowerLimitTextbox);
+    await user.type(populationLowerLimitTextbox, '200');
+    const populationUpperLimitTextbox = screen.getByRole('spinbutton', { name: '最大：' });
+    await user.clear(populationUpperLimitTextbox);
+    await user.type(populationUpperLimitTextbox, '500');
+
+    expect(screen.getByRole('group', { name: '離島設定' })).toBeInTheDocument();
+    await user.click(screen.getByLabelText('離島のみ'));
+
+    await user.type(screen.getByRole('textbox', { name: 'キーワード絞り込み' }), '佐井村');
+
+    await user.click(screen.getByRole('button', { name: 'デフォルト値に戻す' }));
+    await user.click(screen.getByRole('button', { name: '閉じる' }));
+
+    const params = new URLSearchParams({
+      region: '青森県',
+      populationLowerLimit: '1',
+      populationUpperLimit: '10000',
+      islandSetting: '離島を含まない',
+      keyWords: '',
+      page: '1'
+    });
+
+    await user.click(screen.getByRole('button', { name: '探索' }));
+    expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
+  })
 });
