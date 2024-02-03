437cef50e6a2ddd380a485ef778b68df6efae3ab
tomo <o_tomo_1990@yahoo.co.jp>
2024-02-03 21:18:11 +0900

    検索フォームの初期値が反映されていることのテスト

--- __tests__/components/VillageSearchForm.test.tsx
+++ __tests__/components/VillageSearchForm.test.tsx
@@ -117,4 +117,43 @@ describe('VillageSearchForm', () => {
     await user.click(screen.getByRole('button', { name: '探索' }));
     expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
   })
+
+  it('propsがフォームの初期値に反映されている', async () => {
+    render(<VillageSearchForm
+      inputRegion='青森県'
+      inputPopulationLowerLimit='200'
+      inputPopulationUpperLimit='500'
+      inputIslandSetting='離島のみ'
+      inputKeyWords='佐井村'
+    />);
+
+    expect(screen.getByText('青森県')).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: '詳細条件' }));
+
+    expect(screen.getByRole('group', { name: '人口' })).toBeInTheDocument();
+    const populationLowerLimitTextbox = screen.getByRole('spinbutton', { name: '最小：' });
+    expect(populationLowerLimitTextbox).toHaveValue(200);
+    const populationUpperLimitTextbox = screen.getByRole('spinbutton', { name: '最大：' });
+    expect(populationUpperLimitTextbox).toHaveValue(500);
+
+    expect(screen.getByRole('group', { name: '離島設定' })).toBeInTheDocument();
+    expect(screen.getByLabelText('離島のみ')).toBeChecked();
+
+    expect(screen.getByRole('textbox', { name: 'キーワード絞り込み' })).toHaveValue('佐井村');
+
+    await user.click(screen.getByRole('button', { name: '閉じる' }));
+
+    const params = new URLSearchParams({
+      region: '青森県',
+      populationLowerLimit: '200',
+      populationUpperLimit: '500',
+      islandSetting: '離島のみ',
+      keyWords: '佐井村',
+      page: '1'
+    });
+
+    await user.click(screen.getByRole('button', { name: '探索' }));
+    expect(mockFn).toHaveBeenCalledWith(`/result?${params.toString()}`);
+  })
 });
