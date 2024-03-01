00d7fe2ba24706289fb9886e5a19559735db7087
tomo <o_tomo_1990@yahoo.co.jp>
2024-03-01 21:52:38 +0900

    秘境郵便局探索のE2Eテスト

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,7 +1,9 @@
+import { mockPostOffices } from './mockPostOffices';
 import { mockVillages } from './mockVillages';
 
 async function globalSetup() {
   await mockVillages();
+  await mockPostOffices();
 }
 
 export default globalSetup;
