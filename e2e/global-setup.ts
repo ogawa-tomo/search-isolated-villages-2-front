079c6e300ab5e5a4b9cdcc78b1ba4b32983cca57
tomo <o_tomo_1990@yahoo.co.jp>
2024-04-04 22:03:16 +0900

    秘境集落占いと秘境施設占いのE2Eテスト

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,3 +1,4 @@
+import { mockPostOfficeFortune } from './mockPostOfficeFortune';
 import { mockPostOffices } from './mockPostOffices';
 import { mockVillageFortune } from './mockVillageFortune';
 import { mockVillages } from './mockVillages';
@@ -6,6 +7,7 @@ async function globalSetup() {
   await mockVillages();
   await mockPostOffices();
   await mockVillageFortune();
+  await mockPostOfficeFortune();
 }
 
 export default globalSetup;
