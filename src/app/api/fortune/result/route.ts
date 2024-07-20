e5a470c3a48dfbbd71c52adbe86d2e0ae457e6b8
tomo <o_tomo_1990@yahoo.co.jp>
2024-07-21 00:18:09 +0900

    占いの結果をサーバーサイドから取得する

---
+++ src/app/api/fortune/result/route.ts
@@ -0,0 +1,13 @@
+import Village from '@/types/village';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_VILLAGE_API_URL}/api/fortune/result`,
+    {
+      cache: 'no-store',
+    }
+  );
+  const data: Village = await res.json();
+  return NextResponse.json(data);
+}
