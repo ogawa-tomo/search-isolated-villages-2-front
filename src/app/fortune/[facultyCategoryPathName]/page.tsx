ad292009f32685d37e5b0d9dc158e6d000f61f9a
tomo <o_tomo_1990@yahoo.co.jp>
2024-08-04 20:30:33 +0900

    施設探索ページと施設占いページにロゴをつける

--- src/app/fortune/[facultyCategoryPathName]/page.tsx
+++ src/app/fortune/[facultyCategoryPathName]/page.tsx
@@ -1,15 +1,24 @@
 import FacultyFortuneModal from "@/components/FacultyFortuneModal";
 import { getFacultyCategoryFromPathName } from "@/lib/facultyCategories";
+import { facultyCategoryLogo } from "@/lib/facultyCategoryLogo";
 import { FacultyCategoryPathName } from "@/types/FacultyCategory";
+import Image from "next/image";
 
 export default function Page({ params }: { params: { facultyCategoryPathName: FacultyCategoryPathName } }) {
-  const facultyName = getFacultyCategoryFromPathName(params.facultyCategoryPathName).name
+  const facultyCategoryName = getFacultyCategoryFromPathName(params.facultyCategoryPathName).name
 
   return (
     <>
-      <h1 className="text-center">秘境{facultyName}占い</h1>
+      <h1 className="text-center">秘境{facultyCategoryName}占い</h1>
+      <Image
+        className="m-auto"
+        src={facultyCategoryLogo(facultyCategoryName)}
+        alt="logo"
+        width={200}
+        height={200}
+      />
       <p className="text-center">
-        今日のラッキー秘境{facultyName}を占います。
+        今日のラッキー秘境{facultyCategoryName}を占います。
       </p>
 
       <FacultyFortuneModal
