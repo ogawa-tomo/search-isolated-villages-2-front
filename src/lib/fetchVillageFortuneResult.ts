0bb18c72a76cd05109f0acb96e15a6747b98e448
tomo <o_tomo_1990@yahoo.co.jp>
2024-09-15 21:02:07 +0900

    APIをserver actionsにする

--- src/lib/fetchVillageFortuneResult.ts
+++ src/lib/fetchVillageFortuneResult.ts
@@ -1,6 +1,14 @@
+"use server";
+
 import Village from "@/types/Village";
 
 export const fetchVillageFortuneResult = async (): Promise<Village> => {
-  const res = await fetch("/api/fortune/result");
-  return res.json();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_VILLAGE_API_URL}/api/fortune/result`,
+    {
+      cache: "no-store",
+    },
+  );
+  if (!response.ok) throw response;
+  return response.json();
 };
