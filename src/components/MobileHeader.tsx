ea136b0748347a5804dc03ae36f87770c5b48cd2
tomo <o_tomo_1990@yahoo.co.jp>
2025-06-17 22:01:49 +0900

    サイドバーを出す

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { CiSearch } from "react-icons/ci";
 import headerLogo from "@/public/header_logo.png";
+import { HeaderMenu } from "@/app/_components/HeaderMenu";
 
 export const MobileHeader = ({
   onClickSearch,
@@ -10,7 +11,8 @@ export const MobileHeader = ({
 }) => {
   return (
     <>
-      <div className="flex p-2">
+      <div className="flex items-center p-1">
+        <HeaderMenu />
         <Link href="/">
           <Image src={headerLogo} alt="秘境集落探索ツール" height="32" />
         </Link>
